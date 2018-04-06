63e7eb88f1b074bb1387f1b9e7d73b467a2fbc5f
otteheng <otteheng@gmail.com>
2018-04-06 12:12:38 -0400

    Add heroku files

---
+++ Graph 1/app.py
@@ -0,0 +1,174 @@
+# Dependencies for project
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.plotly as py
+import pandas as pd
+from dash.dependencies import Input, Output, State
+import flask
+import os
+from random import randint
+
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+# df = append
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/otteheng/Immigrant-Project/master/Graph%201/clean_national_topstates.csv')
+
+states = list(df['state'].unique())
+
+# Organize where items will be on the page
+app.layout = html.Div([
+    html.H3(
+        children='Distribution of Immigrant Generation among Hispanics and Whites, Nationally \
+                        and in Top Immigration States',
+        style={
+            'textAlign': 'center', 'fontFamily': 'Georgia'
+        }
+    ),
+    html.Center([
+        html.Div([
+            html.Div([html.P('Select State', id='state-title')],
+                     style={'textAlign': 'center', 'fontFamily': 'Georgia'}),
+            dcc.Dropdown(
+                id='state-id',
+                options=[{'label': i, 'value': i} for i in states],
+                value='California')
+        ], style={'width': '50%', 'textAlign': 'center', 'fontFamily': 'Georgia', 'display': 'inline-block'}),
+    ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(id='indicator-graphic1',
+                      config={'modeBarButtonsToRemove': ['sendDataToCloud', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                         'zoom2d', 'resetScale2d'],
+                              'displaylogo': False})
+        ], style={'width': '50%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Graph(id='indicator-graphic2',
+                      config={'modeBarButtonsToRemove': ['sendDataToCloud', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                         'zoom2d', 'resetScale2d'],
+                              'displaylogo': False})
+        ], style={'width': '50%', 'display': 'inline-block'}),
+    ]),
+])
+
+
+@app.callback(
+
+    dash.dependencies.Output('indicator-graphic1', 'figure'),
+    [dash.dependencies.Input('state-id', 'value')])
+def outcome_hispanic(state_id):
+    dff = df[['year', 'wbhao_igen', 'state', 'Share of 1st Generation', 'Share of 2nd Generation',
+              'Share of 3rd Generation']]
+    dff = dff[(dff.state == state_id) & (dff.wbhao_igen == 'Hispanic All')]
+
+    lines = {}
+    data = []
+    y_axis = {'title': '% in {0}'.format(state_id),
+              'hoverformat': ',.2f',
+              'range': [0, 1]}
+    legends = {'orientation': 'h', 'xanchor': 'center', 'x': '0.5', 'y': '-0.22'}
+
+    outcomes = ['Share of 1st Generation', 'Share of 2nd Generation', 'Share of 3rd Generation']
+    for gen in outcomes:
+        if '1st' in gen:
+            lines = dict(
+                color=("#6b6ecf"),
+                width=3)
+        if '2nd' in gen:
+            lines = dict(
+                color=("#80b1d3"),
+                width=3)
+        if '3rd' in gen:
+            lines = dict(
+                color=("#fdb462"),
+                width=3)
+        trace = go.Scatter(
+            x=dff['year'],
+            y=dff[gen],
+            mode='lines',
+            name=gen,
+            line=lines,
+            opacity=0.8
+        )
+
+        data.append(trace)
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title='Hispanic',
+            titlefont=dict(
+                family='Georgia'),
+            xaxis={'title': 'Year'},
+            yaxis=y_axis,
+            legend=legends
+        )
+    }
+
+
+@app.callback(
+
+    dash.dependencies.Output('indicator-graphic2', 'figure'),
+    [dash.dependencies.Input('state-id', 'value')])
+def outcome_white(state_id):
+    dff = df[['year', 'wbhao_igen', 'state', 'Share of 1st Generation', 'Share of 2nd Generation',
+              'Share of 3rd Generation']]
+    dff = dff[(dff.state == state_id) & (dff.wbhao_igen == 'White All')]
+
+    lines = {}
+    data = []
+    y_axis = {'title': '% in {0}'.format(state_id),
+              'hoverformat': ',.2f',
+              'range': [0, 1]}
+    legends = {'orientation': 'h', 'xanchor': 'center', 'x': '0.5', 'y': '-0.22'}
+
+    outcomes = ['Share of 1st Generation', 'Share of 2nd Generation', 'Share of 3rd Generation']
+    for gen in outcomes:
+        if '1st' in gen:
+            lines = dict(
+                color=("#6b6ecf"),
+                width=3)
+        if '2nd' in gen:
+            lines = dict(
+                color=("#80b1d3"),
+                width=3)
+        if '3rd' in gen:
+            lines = dict(
+                color=("#fdb462"),
+                width=3)
+        trace = go.Scatter(
+            x=dff['year'],
+            y=dff[gen],
+            mode='lines',
+            name=gen,
+            line=lines,
+            opacity=0.8
+        )
+
+        data.append(trace)
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title='White',
+            titlefont=dict(
+                family='Georgia'),
+            xaxis={'title': 'Year'},
+            yaxis=y_axis,
+            legend=legends
+        )
+    }
+
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.server.run(debug=True, threaded=True)
---
+++ Graph 3/app.py
@@ -0,0 +1,232 @@
+# Dependencies for project
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import plotly.plotly as py
+import pandas as pd
+from dash.dependencies import Input, Output, State
+import flask
+import os
+from random import randint
+
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+# df = gender
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/otteheng/Immigrant-Project/master/Graph%203/clean_national_gender.csv')
+
+outcomes = ["% less than High School diploma", "% with College Degree", "% with Health Insurance", "% Employed",
+            "% Married", "% with Children", "Median Individual Real Income"]
+
+# Organize where items will be on the page
+app.layout = html.Div([
+    html.H3(
+        children='Cross-Generational Differences in Hispanic Outcomes by Gender, 1994-2016',
+        style={
+            'textAlign': 'center', 'fontFamily': 'Georgia'
+        }
+    ),
+    html.Center([
+        html.Div([
+            html.Div([html.P('Select Outcome', id='outcome-title')],
+                     style={'textAlign': 'center', 'fontFamily': 'Georgia'}),
+            dcc.Dropdown(
+                id='outcome-id',
+                options=[{'label': i, 'value': i} for i in outcomes],
+                value='% less than High School diploma')
+        ], style={'width': '50%', 'textAlign': 'center', 'fontFamily': 'Georgia', 'display': 'inline-block'}),
+    ]),
+    html.Div([
+        html.Div([
+            dcc.Graph(id='indicator-graphic1',
+                      config={'modeBarButtonsToRemove': ['sendDataToCloud', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                         'zoom2d', 'resetScale2d'],
+                              'displaylogo': False})
+        ], style={'width': '50%', 'display': 'inline-block'}),
+        html.Div([
+            dcc.Graph(id='indicator-graphic2',
+                      config={'modeBarButtonsToRemove': ['sendDataToCloud', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                         'zoom2d', 'resetScale2d'],
+                              'displaylogo': False})
+        ], style={'width': '50%', 'display': 'inline-block'}),
+    ]),
+])
+
+
+@app.callback(
+
+    dash.dependencies.Output('indicator-graphic1', 'figure'),
+    [dash.dependencies.Input('outcome-id', 'value')])
+def outcome_male(outcome_id):
+    dff = df[['year', 'wbhao_igen', 'female', outcome_id]]
+    dff = dff[(dff.female == 0)]
+
+    lines = {}
+    data = []
+
+    m_max = dff[outcome_id].max()
+    m_min = dff[outcome_id].min()
+    f = df[['year', 'wbhao_igen', 'female', outcome_id]]
+    f = f[(f.female == 1)]
+    f_max = f[outcome_id].max()
+    f_min = f[outcome_id].min()
+    if m_max > f_max:
+        df_max = m_max
+    elif m_max < f_max:
+        df_max = f_max
+    if m_min > f_min:
+        df_min = f_min
+    elif m_min < f_min:
+        df_min = m_min
+    if df_min < 0.05:
+        df_min = 0
+
+    y_axis = {'title': '{0}'.format(outcome_id),
+              'hoverformat': ',.2f',
+              'range': [df_min, df_max]}
+    legends = {'orientation': 'h', 'xanchor': 'center', 'x': '0.5', 'y': '-0.22'}
+
+    # Show three lines for each output
+    generation = ['White All', 'Hispanic All', 'Hispanic 1st Generation', 'Hispanic 2nd Generation',
+                  'Hispanic 3rd Generation']
+    for gen in generation:
+        if '1st' in gen:
+            lines = dict(
+                color=("#6b6ecf"),
+                width=2,
+                dash='dash')
+        if '2nd' in gen:
+            lines = dict(
+                color=("#80b1d3"),
+                width=2,
+                dash='dash')
+        if '3rd' in gen:
+            lines = dict(
+                color=("#fdb462"),
+                width=2,
+                dash='dash')
+        if 'White All' in gen:
+            lines = dict(
+                color=("#333333"),
+                width=3)
+        if 'Hispanic All' in gen:
+            lines = dict(
+                color=("#fb8072"),
+                width=3)
+        trace = go.Scatter(
+            x=dff[(dff.wbhao_igen == gen)]['year'],
+            y=dff[(dff.wbhao_igen == gen)][outcome_id],
+            mode='lines',
+            name=gen,
+            line=lines,
+            opacity=0.8
+        )
+        data.append(trace)
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title='Male',
+            titlefont=dict(
+                family='Georgia'),
+            xaxis={'title': 'Year'},
+            yaxis=y_axis,
+            legend=legends
+        )
+    }
+
+
+@app.callback(
+
+    dash.dependencies.Output('indicator-graphic2', 'figure'),
+    [dash.dependencies.Input('outcome-id', 'value')])
+def outcome_female(outcome_id):
+    dff = df[['year', 'wbhao_igen', 'female', outcome_id]]
+    dff = dff[(dff.female == 1)]
+
+    lines = {}
+    data = []
+
+    f_max = dff[outcome_id].max()
+    f_min = dff[outcome_id].min()
+    m = df[['year', 'wbhao_igen', 'female', outcome_id]]
+    m = m[(m.female == 0)]
+    m_max = m[outcome_id].max()
+    m_min = m[outcome_id].min()
+    if f_max > m_max:
+        df_max = f_max
+    elif f_max < m_max:
+        df_max = m_max
+    if f_min > m_min:
+        df_min = m_min
+    elif f_min < m_min:
+        df_min = f_min
+    if df_min < 0.05:
+        df_min = 0
+
+    y_axis = {'title': '{0}'.format(outcome_id),
+              'hoverformat': ',.2f',
+              'range': [df_min, df_max]}
+    legends = {'orientation': 'h', 'xanchor': 'center', 'x': '0.5', 'y': '-0.22'}
+
+    # Show three lines for each output
+    generation = ['White All', 'Hispanic All', 'Hispanic 1st Generation', 'Hispanic 2nd Generation',
+                  'Hispanic 3rd Generation']
+    for gen in generation:
+        if '1st' in gen:
+            lines = dict(
+                color=("#6b6ecf"),
+                width=2,
+                dash='dash')
+        if '2nd' in gen:
+            lines = dict(
+                color=("#80b1d3"),
+                width=2,
+                dash='dash')
+        if '3rd' in gen:
+            lines = dict(
+                color=("#fdb462"),
+                width=2,
+                dash='dash')
+        if 'White All' in gen:
+            lines = dict(
+                color=("#333333"),
+                width=3)
+        if 'Hispanic All' in gen:
+            lines = dict(
+                color=("#fb8072"),
+                width=3)
+        trace = go.Scatter(
+            x=dff[(dff.wbhao_igen == gen)]['year'],
+            y=dff[(dff.wbhao_igen == gen)][outcome_id],
+            mode='lines',
+            name=gen,
+            line=lines,
+            opacity=0.8
+        )
+        data.append(trace)
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            title='Female',
+            titlefont=dict(
+                family='Georgia'),
+            xaxis={'title': 'Year'},
+            yaxis=y_axis,
+            legend=legends
+        )
+    }
+
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.server.run(debug=True, threaded=True)
